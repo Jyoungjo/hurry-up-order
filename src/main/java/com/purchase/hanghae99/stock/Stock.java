f574ec2afaaae8c14bd33d7440e00f528b5586ce
Jyoungjo <zimbote0411@gmail.com>
2024-06-24 15:33:23 +0900

    refactor: 재고 처리를 위한 Entity 구조 변경 - Stock 엔티티 생성 - Item : Stock = 1 : 1 연관관계 매핑

--- src/main/java/com/purchase/hanghae99/stock/Stock.java
+++ src/main/java/com/purchase/hanghae99/stock/Stock.java
@@ -1,6 +1,7 @@
-package com.purchase.hanghae99.item;
+package com.purchase.hanghae99.stock;
 
 import com.purchase.hanghae99.common.BaseEntity;
+import com.purchase.hanghae99.item.Item;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -12,18 +13,19 @@ import org.hibernate.annotations.SQLRestriction;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "TB_ITEM")
+@Table(name = "TB_STOCK")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
-@SQLDelete(sql = "UPDATE TB_ITEM SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
+@SQLDelete(sql = "UPDATE TB_STOCK SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
 @SQLRestriction("deleted_at is NULL")
-public class Item extends BaseEntity {
+public class Stock extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private String description;
-    private Integer price;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="item_id")
+    private Item itemId;
+    private Integer quantity;
     private LocalDateTime deletedAt;
 }
